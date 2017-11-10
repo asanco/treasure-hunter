de56d60b56b1319bc3b87d1ae459984e758a97c9
Andrés Sandoval <a.sandoval1303@uniandes.edu.co>
2017-11-09 22:25:23 -0500

    Initial commit

---
+++ client/main.js
@@ -0,0 +1,28 @@
+import React, { PropTypes } from 'react';
+import { render } from 'react-dom';
+import MyMap from '../imports/MyMap';
+
+if (Meteor.isClient) {
+  class App extends React.Component {
+    constructor() {
+      super();
+      this.state = { open: true };
+    }
+
+    render() {
+      return (
+        <div>
+          <button onClick={() => this.setState({ open: !this.state.open })}>
+            Toggle
+          </button>
+          <hr />
+          {this.state.open ? <MyMap /> : null}
+        </div>
+      );
+    }
+  }
+
+  Meteor.startup(() => {
+    render(<App />, document.getElementById('render-target'));
+  });
+}
