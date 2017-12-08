11fa76d4fe49ea5bdc4158956f5964c6d50b7d42
Andrés Sandoval <a.sandoval1303@uniandes.edu.co>
2017-12-08 16:06:03 -0500

    Added icons and navbar changes

--- imports/ui/NavBar.js
+++ imports/ui/NavBar.js
@@ -6,13 +6,15 @@ class NavBar extends Component {
     return (
       <div >
       <nav className='navbar sticky-top navbar-dark navbar navbar-expand-lg'>
-        <a className='navbar-brand' href='#home'>T.H.</a>
+        <img height='64' width='64' alt='Website icon of a map' src='../../treasure-map.png' />
+        <a className='navbar-brand' href='#home'>Treasure Hunter</a>
         <ul className='navbar-nav'>
-          <li className='nav-item'><a className='nav-link' href='#home'>Home</a></li>
-          <li className='nav-item'><a className='nav-link' href='#design'>Design</a></li>
+
         </ul>
 
         <ul className='nav navbar-nav ml-auto'>
+          <li className='nav-item'><a className='nav-link' href='#home'>Home</a></li>
+          <li className='nav-item'><a className='nav-link' href='#design'>Create Hunt</a></li>
           <li className='nav-item'><a className='nav-link' href='#login'>Login</a></li>
         </ul>
       </nav>
